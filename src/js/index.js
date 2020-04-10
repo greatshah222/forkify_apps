379d9ebaa25713a1004f187165fb8a888e55d493
greatshah222 <great.shah222@gmail.com>
2020-04-10 18:54:01 +0300

    rendering of recipe from api

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 
 import { elements, renderLoader, clearLoader } from './views/base';
 
@@ -121,6 +122,8 @@ const controlRecipe = async () => {
 
     if (id) {
         // prepare the ui for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
 
 
         // create new recipe object
@@ -130,8 +133,8 @@ const controlRecipe = async () => {
             // get recipe data and parse ingrediets
             await state.recipe.getRecipe();
             // console.log(state.recipe.ingredients);
-           state.recipe.parseIngredients();
-           
+            state.recipe.parseIngredients();
+
 
             // calculate servings and time 
             state.recipe.calcTime();
@@ -140,10 +143,13 @@ const controlRecipe = async () => {
 
             // render recipe 
 
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         }
         catch (error) {
-          // alert('something went wrong in the main file ');
+            // alert('something went wrong in the main file ');
+            console.log(error);
+            alert('error proccessing recipe');
 
         }
 
